edce5a917cfb733ef935d9204cf7b3dd56c037e2
aschultz10 <77344570+aschultz10@users.noreply.github.com>
2021-05-13 14:16:48 -0400

    data issue

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,24 +1,81 @@
-// Use the D3 library to read in samples.json.
-// Get the data using function
+// Create H Grah, Filter, Bubble Chart, Plot, then Dropdown
 
-function grabdata(id) {
-      d3.json("samples.json").then((data) => {
+// Creating Horizontal Graph, start function and pull data with d3
 
-// Using d3 create variables based on what is needed in the horizontal bar chart
-       
-        var sample_values = data.samples;
+function horizontal(id) {
+    d3.json("samples.json").then((data) => {
+      var sample = data.samples;
+
+// Create sample_values, otu_ids, otu_labels variables, we want the first ones [0] 
+      var ids = sample.filter(x => x.id == id);
+      var otu_ids = ids[0].otu_ids;
+      var otu_labels = ids[0].otu_labels;
+      var otu_values = ids[0].sample;
+
+// Y values
+      var yaxis = otu_ids.slice(0, 10).map(x => "OTU" + x).reverse()
+
+// Start defining the datatrace with variables we created above
+      var trace = {
         
-// Filter for only IDs using " x => " method
-    var filtered = sample_values.filter(x=> x.id === id); 
+//Filter for Top Ten with slice reverse
+        x: otu_values.slice(0, 10).reverse(),     
+        y: yaxis,    
+        text: otu_labels.slice(0, 10).reverse(),     
+        type: "bar", 
+        orientation: "h"
+      };
+      var layout = {
+        title: "Bacteria found"
+      };
+      var data = [trace];
+      Plotly.newPlot("bar", data, layout);
+  
+// Create bubble chart variable, add appropriate variables from above 
 
-// Use Filtered variable to define variables with correct IDs
-    var otu_ids = filtered[0].otu_ids;
-    var otu_labels = filtered[0].otu_labels;
-    var otu_xvalues = filtered[0].otu_xvalues;
-    var otu_yvalues = filtered[0].otu_yvalues;
+      var bubbleData = {
+        x: otu_ids,
+        y: otu_values,
+        text: otu_labels,
+        mode: "markers",
+        marker: {
+          size: otu_values,
+          color: otu_ids,
+          colorscale: "Earth"
+        }
+      }
+// Create layout for bubble chart
+      var bubbleLayout = {
+        title: "Bacteria cultures per sample",
+        margin: {
+          t: 30
+        },
+        hovermode: "closest",
+        xaxis: {
+          title: "OTU Id"
+        }
+      }
 
-    var datatrace = {
-        
-    }
+// Plot bubble chart      
+      Plotly.newPlot("bubble", [bubbleData], bubbleLayout)
+    });
+  };
+  
 
-      }  
+function dropdown() {
+    var dropdown = d3.select("#selDataset");
+    // Pull data w d3
+    d3.json("data/samples.json").then((data)=> {
+        console.log(data)
+        data.names.forEach(function(name) {
+            dropdown.
+            append("option").
+            text(name).
+            property("value");
+        });
+        getPlot(data.names[0]);
+        getInfo(data.names[0]);
+    });
+}
+
+
